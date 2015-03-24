a9157f82b6c5bc8bf601ef62be56a5235e754c06
Gregory Petukhov <lorien@lorien.name>
2015-03-24 22:33:13 +0600

    Add test_item script

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 import os.path
 
 ROOT = os.path.dirname(os.path.realpath(__file__))
@@ -12,7 +12,7 @@ setup(
     author = 'Gregory Petukhov',
     author_email = 'lorien@lorien.name',
     install_requires = ['selection', 'tools'],
-    packages = ['item', 'script', 'test'],
+    packages = ['item', 'item.script', 'script', 'test'],
     license = "MIT",
     classifiers = (
         'Programming Language :: Python',
